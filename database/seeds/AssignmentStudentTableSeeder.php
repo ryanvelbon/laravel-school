509941cc774e3cb20a02660425da55a6679d7f41
ryanvb92 <ryanvellagi@gmail.com>
2019-06-01 22:30:05 +0200

    Seeder created.

---
+++ database/seeds/AssignmentStudentTableSeeder.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class AssignmentStudentTableSeeder extends Seeder
+{
+    public function run()
+    {
+        DB::table('assignment_student')->delete();
+
+		$groups = App\Group::all();
+
+		foreach($groups as $group){
+
+			$assignments = App\Assignment::where('subject_id', $group->subject_id)
+										->where('level_id', $group->level_id)
+										->get();
+			
+			foreach ($assignments as $assignment) {
+				foreach($group->students as $student){
+
+					$submitted = random_int(0, 1);
+
+					$mark = ($submitted) ? random_int(0, $assignment->marks_available) : 0 ;
+
+					DB::table('assignment_student')->insert(
+						['assignment_id' => $assignment->id,
+						 'student_id' => $student->id,
+						 'deadline' => date('Y-m-d', random_int(1546297200, 1559340000)),
+						 'submitted' => $submitted,
+						 'mark' => $mark,
+					]);
+				}
+			}
+		}
+    }
+}
